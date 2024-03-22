488c3dc61395ad542370fe70d083228c1ded01ec
mguerra96 <marco.guerra@ingv.it>
2024-03-22 14:47:20 +0100

    increased perfomance
    Changed string arcId to categorical to icnerase speed

--- Functions/Calibration_Functions/Verticalization.m
+++ Functions/Calibration_Functions/Verticalization.m
@@ -1,5 +1,5 @@
 function vtec=Verticalization(stec,ele,Hipp)
-% SBAGLIATO NON CI DEVE ESSERE HIPP IN QUESTA FORMULA
+
 vtec=stec.*cos(asin(6371/(6371+Hipp)*cosd(ele)));
 
 end
--- Functions/Softwares_And_Utils/SettingsManager.m
+++ Functions/Softwares_And_Utils/SettingsManager.m
@@ -1,4 +1,4 @@
-function [t_res,HIPP,Elevation_Cutoff,GNSS_Systems,ToCalibrate,ToVertical,MinArcLength]=SettingsManager(MyIonoSettings)
+function [t_res,HIPP,Elevation_Cutoff,GNSS_Systems,ToNeCalibrate,ToVertical,MinArcLength]=SettingsManager(MyIonoSettings)
 
 %function that handles the non madnatory settings and if they are not provided assigns default values
 
@@ -9,7 +9,7 @@ else
 end
 
 if ~isfield(MyIonoSettings,'HIPP')
-    HIPP=250;
+    HIPP=300;
 else
     HIPP=MyIonoSettings.HIPP;
 end
@@ -21,15 +21,15 @@ else
 end
 
 if ~isfield(MyIonoSettings,'GNSSSystems')
-    GNSS_Systems=["G","R","E","C","S"];
+    GNSS_Systems=["G","R","E","C"];
 else
     GNSS_Systems=MyIonoSettings.GNSSSystems;
 end
 
-if ~isfield(MyIonoSettings,'ToCalibrate')
-    ToCalibrate=0;
+if ~isfield(MyIonoSettings,'ToNeCalibrate')
+    ToNeCalibrate=0;
 else
-    ToCalibrate=MyIonoSettings.ToCalibrate;
+    ToNeCalibrate=MyIonoSettings.ToNeCalibrate;
 end
 
 if ~isfield(MyIonoSettings,'ToVertical')
